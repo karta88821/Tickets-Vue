2ef622015d82fb0d4200cf0cfffe712ff10f8519
ubuntu <karta88821@gmail.com>
2023-02-15 13:55:13 +0800

    add container in the index page

---
+++ src/views/Index.vue
@@ -0,0 +1,88 @@
+<template>
+    <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-container>
+            <el-header>
+                <strong>後台管理系統</strong>
+                <div class="header-avatar">
+                    <el-avatar
+                        size="medium"
+                        src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+                    ></el-avatar>
+                    <el-dropdown>
+                        <span class="el-dropdown-link">
+                            Admin<i
+                                class="el-icon-arrow-down el-icon--right"
+                            ></i>
+                        </span>
+                        <el-dropdown-menu slot="dropdown">
+                            <el-dropdown-item>個人中心</el-dropdown-item>
+                            <el-dropdown-item>登出</el-dropdown-item>
+                        </el-dropdown-menu>
+                    </el-dropdown>
+                    <el-link href="" target="_blank">網站</el-link>
+                </div>
+            </el-header>
+            <el-main>Main</el-main>
+        </el-container>
+    </el-container>
+</template>
+
+<script>
+export default {
+    name: "Index",
+};
+</script>
+
+<style>
+.el-container {
+    padding: 0;
+    margin: 0;
+    height: 100%;
+}
+.header-avatar {
+    float: right;
+    width: 210px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+}
+.el-header,
+.el-footer {
+    background-color: #b3c0d1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+}
+
+.el-aside {
+    background-color: #d3dce6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+}
+
+.el-main {
+    background-color: #e9eef3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+}
+
+body > .el-container {
+    margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+}
+
+.el-dropdown-link {
+    cursor: pointer;
+}
+</style>
